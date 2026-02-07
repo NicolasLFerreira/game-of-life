2eb4b71966ffe1dde43435038d4221263e051b6b
nicolaslferreira <nicolas.l.ferreira@gmail.com>
2026-02-07 21:39:32 +1300

    barebone functional implementation completed

--- src/main.rs
+++ src/main.rs
@@ -225,14 +225,29 @@ fn main() {
     .unwrap();
 }
 
-#[derive(Default)]
+struct SimulationState {
+    cells: Grid,
+    is_running: bool,
+    step_once: bool,
+}
+
 struct App {
     grid_pan: egui::Vec2,
+    show_grid: bool,
+    sim_state: SimulationState,
 }
 
 impl App {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        Self::default()
+        Self {
+            grid_pan: egui::Vec2::default(),
+            show_grid: true,
+            sim_state: SimulationState {
+                cells: Grid::new(),
+                step_once: false,
+                is_running: false,
+            },
+        }
     }
 }
 
@@ -259,9 +274,16 @@ impl eframe::App for App {
             ui.heading("Simulation Statistics");
         });
         egui::SidePanel::right("tools").show(ctx, |ui| {
-            ui.heading("Simulation Tools");
+            ui.heading("Grid tools");
+            ui.checkbox(&mut self.show_grid, "Show Grid");
+            ui.heading("Simulation tools");
+            ui.checkbox(&mut self.sim_state.is_running, "Run");
             if ui.button("Step").clicked() {
-                println!("Stepping");
+                self.sim_state.step_once = true;
+            }
+            if ui.button("Clear").clicked() {
+                self.sim_state.cells = Grid::new();
+                self.grid_pan = egui::Vec2::default();
             }
         });
         egui::CentralPanel::default().show(ctx, |ui| {
@@ -271,29 +293,80 @@ impl eframe::App for App {
             // display geometry values
             let viewport = response.rect;
             let anchor = viewport.min;
-            let center = anchor + viewport.size() * 0.5;
+            let size = viewport.size();
+            let center = anchor + size * 0.5;
 
             // store panning
             if response.dragged() {
-                self.grid_pan += response.drag_delta();
-                println!("dragged {:?}", self.grid_pan)
+                let input = response.ctx.input(|i| i.clone());
+                if input.pointer.button_down(egui::PointerButton::Middle) {
+                    self.grid_pan += response.drag_delta();
+                }
             }
 
-            // store click
-            if response.clicked() {
-                let click = response.interact_pointer_pos().unwrap();
+            if response.clicked() || response.dragged() {
+                let input = response.ctx.input(|i| i.clone());
 
-                let normalized = (
-                    ((click.x - center.x - self.grid_pan.x) / CELL_SIZE_PX).floor() as i32,
-                    ((click.y - center.y - self.grid_pan.y) / CELL_SIZE_PX).floor() as i32,
+                let Some(click) = response.interact_pointer_pos() else {
+                    return;
+                };
+
+                let x = ((click.x - center.x - self.grid_pan.x) / CELL_SIZE_PX).floor() as i32;
+                let y = ((click.y - center.y - self.grid_pan.y) / CELL_SIZE_PX).floor() as i32;
+
+                if input.pointer.button_down(egui::PointerButton::Primary) {
+                    self.sim_state.cells.insert((x, y));
+                } else if input.pointer.button_down(egui::PointerButton::Secondary) {
+                    self.sim_state.cells.remove(&(x, y));
+                }
+            }
+
+            // EXTREMELY unoptimized, however, it works, so I'll leave it here for now
+            if self.show_grid {
+                let start_x = anchor.x as i32;
+                let start_y = anchor.y as i32;
+                let end_x = size.x as i32;
+                let end_y = size.y as i32;
+
+                for x in start_x..end_x {
+                    for y in start_y..end_y {
+                        paint_cell(
+                            &painter,
+                            center,
+                            x - center.x as i32,
+                            y - center.y as i32,
+                            self.grid_pan,
+                            if (x + y) % 2 == 0 {
+                                egui::Color32::LIGHT_GRAY
+                            } else {
+                                egui::Color32::GRAY
+                            },
+                        );
+                    }
+                }
+            } else {
+                // when no grid just show white
+                painter.rect_filled(viewport, 0.0, egui::Color32::WHITE);
+            }
+
+            for cell in self.sim_state.cells.iter() {
+                paint_cell(
+                    &painter,
+                    center,
+                    cell.0,
+                    cell.1,
+                    self.grid_pan,
+                    egui::Color32::BLACK,
                 );
             }
-
-            // Paint white
-            painter.rect_filled(viewport, 0.0, egui::Color32::WHITE);
-
-            paint_cell(&painter, center, 0, 0, self.grid_pan, egui::Color32::BLACK);
         });
+
+        // Sim run, uncapped rn
+        if self.sim_state.is_running || self.sim_state.step_once {
+            self.sim_state.step_once = false;
+            logical_step(&mut self.sim_state.cells);
+            ctx.request_repaint()
+        }
     }
 }
 
